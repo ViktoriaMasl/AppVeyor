224d16f8439a9dd414f60beb60433f1fcb5b40f7
viktoriya maslennikova <libikvikaa@yandex.ru>
2021-01-27 21:06:50 +0500

    Update project v.4

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 
 class MobileBankApiTestV1 {
+
     @Test
     void shouldReturnDemoAccounts() {
       // Given - When - Then
